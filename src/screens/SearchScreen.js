2e75111278ec0c682b0085cc5e30712bcbdccf51
Sumit Arya <sumit.arya@thoughts2binary.com>
2020-11-19 01:11:01 +0530

    search component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,15 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
   return (
     <View>
-      <Text>SearchScreen</Text>
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => console.log("SUBMIT-", term )} />
+      <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
